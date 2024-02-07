2e60d61cf44bdb3144f31440743717c639182e3a
alexamoy <s.alexa.moy@gmail.com>
2024-02-06 19:35:59 -0500

    initial landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Typography } from '@mui/material';
+import logan_pass_img from './static/assets/logan_pass.jpeg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="bg" style={{ backgroundImage: `url(${logan_pass_img})` }}></div>
+        <div className="landing-page-text">
+          <Typography variant="h1" component="h2">
+            Alexa & Luke
+          </Typography>
+          <Typography variant="h4" component="h2">
+            August 15th, 2024
+          </Typography>
+          <Typography variant="h6" component="h2">
+            Glacier National Park
+          </Typography>
+        </div>
     </div>
   );
 }
